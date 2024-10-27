8f145733ac77845f51d2f1bd9e77330f54a2886b
Serhii Mishovych <156013854+SerhiiMis@users.noreply.github.com>
2024-10-27 09:51:02 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import SortCss from 'postcss-sort-media-queries';
 export default defineConfig(({ command }) => {
   return {
     define: {
+      'process.env': process.env,
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
